4b19e5bcbcca7fb5de29cfb7488454e29dfbc011
Evgeny Samoylov <samoyl@gmail.com>
2023-07-30 16:44:50 +0200

    add url and urls html

--- page_analyzer/data.py
+++ page_analyzer/data.py
@@ -1,10 +1,10 @@
 import os
 import datetime
 import psycopg2
-from dotenv import load_dotenv
+#  from dotenv import load_dotenv
 
 
-load_dotenv()  # take environment variables from .env
+#  load_dotenv()  # take environment variables from .env
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
