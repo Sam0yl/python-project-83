4212efc94e045c7de29dc3bb0bfc0763ea88829c
Evgeny Samoylov <samoyl@gmail.com>
2023-08-01 16:04:01 +0200

    refactoring

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -10,19 +10,29 @@ load_dotenv()  # take environment variables from .env
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+def connect_db():
+    connect = psycopg2.connect(DATABASE_URL)
+    return connect
+
+
+def execute_sql_query(connect, query, params=None):
+    with connect.cursor() as curs:
+        curs.execute(query, params)
+        try:
+            result = curs.fetchall()
+            return result
+        except Exception:
+            return
+
+
 class UrlRepository():
 
-    def connect(self):
-        conn = psycopg2.connect(DATABASE_URL)
-        return conn
-
     def is_url_in_repository(self, url):
-        conn = self.connect()
-        with conn.cursor() as curs:
-            curs.execute(
-                'SELECT name FROM urls WHERE name=%s',
-                (url.name,))
-            result = curs.fetchall()
+        conn = connect_db()
+        result = execute_sql_query(
+            conn,
+            'SELECT name FROM urls WHERE name=%s',
+            (url.name,))
         conn.close()
         if not result:
             return False
@@ -30,65 +40,63 @@ class UrlRepository():
 
     def add_url(self, url):
         if not self.is_url_in_repository(url):
-            conn = self.connect()
-            with conn.cursor() as curs:
-                created_at = str(datetime.date.today())
-                curs.execute(
-                    'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
-                    (url.name, created_at))
+            conn = connect_db()
+            created_at = str(datetime.date.today())
+            execute_sql_query(
+                conn,
+                'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
+                (url.name, created_at))
             conn.commit()
             conn.close()
 
     def assign_url_id(self, url):
         if self.is_url_in_repository(url):
-            conn = self.connect()
-            with conn.cursor() as curs:
-                curs.execute(
-                    'SELECT id, created_at FROM urls WHERE name=%s',
-                    (url.name,))
-                url_data = curs.fetchall()
+            conn = connect_db()
+            url_data = execute_sql_query(
+                conn,
+                'SELECT id, created_at FROM urls WHERE name=%s',
+                (url.name,))
             conn.close()
         id, created_at = url_data[0]
         url.id = id
         url.created_at = created_at
 
     def get_url_by_id(self, url_id):
-        conn = self.connect()
-        with conn.cursor() as curs:
-            curs.execute(
-                'SELECT name, created_at FROM urls WHERE id=%s',
-                (url_id,))
-            url_data = curs.fetchall()
+        conn = connect_db()
+        url_data = execute_sql_query(
+            conn,
+            'SELECT name, created_at FROM urls WHERE id=%s',
+            (url_id,))
         conn.close()
         name, created_at = url_data[0]
         return Url(name, url_id, str(created_at))
 
     def get_urls(self):
-        conn = self.connect()
-        with conn.cursor() as curs:
-            curs.execute('SELECT * FROM urls')
-            db_url_data = curs.fetchall()
+        conn = connect_db()
+        urls_data = execute_sql_query(
+            conn,
+            'SELECT * FROM urls')
         conn.close()
-        db_url_data.reverse()
+        urls_data.reverse()
         urls = [Url(name, id, created_at)
-                for id, name, created_at in db_url_data]
+                for id, name, created_at in urls_data]
         for url in urls:
             url.set_last_check(self.get_last_url_check(url.id))
         return urls
 
     def add_url_check(self, check):
-        conn = self.connect()
-        with conn.cursor() as curs:
-            curs.execute(
-                """INSERT INTO url_checks (url_id, created_at, status_code,
-                title, h1, description) VALUES (%s, %s, %s, %s, %s, %s)""",
-                (check.url_id, check.created_at, check.code, check.title,
-                 check.h1, check.description))
+        conn = connect_db()
+        execute_sql_query(
+            conn,
+            """INSERT INTO url_checks (url_id, created_at, status_code,
+            title, h1, description) VALUES (%s, %s, %s, %s, %s, %s)""",
+            (check.url_id, check.created_at, check.code,
+             check.title, check.h1, check.description))
         conn.commit()
         conn.close()
 
     def get_url_checks(self, url_id):
-        conn = self.connect()
+        conn = connect_db()
         with conn.cursor() as curs:
             curs.execute('SELECT * FROM url_checks WHERE url_id=%s', (url_id,))
             db_data = curs.fetchall()
@@ -100,7 +108,7 @@ class UrlRepository():
         return checks
 
     def get_last_url_check(self, url_id):
-        conn = self.connect()
+        conn = connect_db()
         with conn.cursor() as curs:
             curs.execute(
                 'SELECT * FROM url_checks WHERE url_id=%s \
